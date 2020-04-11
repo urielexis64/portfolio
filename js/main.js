9823bd39eed79263b0a2ae652cb05eff5d71dfda
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-04-10 23:54:09 -0600

    Owl Carousel added to About me area

--- js/main.js
+++ js/main.js
@@ -16,7 +16,23 @@ $(document).ready(function () {
 
     $('.project-area .grid .test-popup-link').magnificPopup({
         type: 'image',
-        gallery:{enabled:true}
-      });
+        gallery: { enabled: true }
+    });
+
+    //Owl Carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            544:{
+                items:2
+            }
+        }
+    });
 
 });
