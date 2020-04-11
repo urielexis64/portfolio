9c91fbe3b63f5a4a4ef284bcb0a742b77e8764c8
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-04-10 21:13:42 -0600

    Added carousel in project-area with magnific popup

--- js/main.js
+++ js/main.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
         });
         return false;
     })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+      });
+
 });
